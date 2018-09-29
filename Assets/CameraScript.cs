93680ed07775b305deb25b6d91e83fad19734acd
keiespro <keiespro@aol.com>
2018-09-29 10:37:28 +0300

    tilting fix

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -1,26 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CameraScript : MonoBehaviour
 {
 
     static WebCamTexture backCam;
+    private bool camAvaiable;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
 
     void Start()
     {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.LogError("No Camera Detected");
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            //  if(!devices[i].isFrontFacing)
+            //  {
+            backCam = new WebCamTexture(devices[0].name, Screen.width, Screen.height);
+            // }
+        }
+
         if (backCam == null)
-            backCam = new WebCamTexture();
+        {
+            Debug.LogError("Unable to find camera");
+            return;
 
-        GetComponent<Renderer>().material.mainTexture = backCam;
+        }
 
-        if (!backCam.isPlaying)
-            backCam.Play();
+        backCam.Play();
+        background.texture = backCam;
+
+        camAvaiable = true;
+
+        //GetComponent<Renderer>().material.mainTexture = backCam;
+
+        //if (!backCam.isPlaying)
+        //    backCam.Play();
 
     }
 
-    void Update()
+    private void Update()
     {
+        if (!camAvaiable)
+            return;
 
+        float ratio = (float)backCam.width / (float)backCam.height;
+        fit.aspectRatio = ratio;
+
+        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1, scaleY, 1);
+
+        int orient = -backCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+
+    public void StartOver()
+    {
+        SceneManager.LoadScene(0);
     }
 }
